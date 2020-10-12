8839ab0500a615219b05e22e659fe84a843733a7
Victoria  Topham <topham.victoria@gmail.com>
2020-10-12 09:05:06 -0700

    added a redirect

--- no-more-dead-plants/src/App.js
+++ no-more-dead-plants/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 
 import Header from './Components/Header'
 import Home from './Pages/Home'
@@ -15,6 +15,9 @@ function App() {
       
       <Router>
         <Header/>
+        <Route exact path = "/">
+          <Redirect to = "/home"/>
+        </Route>
         <Route exact path = "/home" component = {Home}/>
         {/* <Route path = "/edit" component = {EditPlant}/> */}
         <Route path = "/add" component = {Home}/>
