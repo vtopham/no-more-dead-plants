ab213c7ddf725554ee8bb910f6c0c2b6b37ea0ce
Victoria  Topham <topham.victoria@gmail.com>
2020-10-12 09:03:22 -0700

    updated dummy data somewhat

--- no-more-dead-plants/src/State/Reducers/reducer.js
+++ no-more-dead-plants/src/State/Reducers/reducer.js
@@ -13,14 +13,14 @@ const defaultState = {
     plants: [
         {
             id: 1,
-            name: 'cutefrillything',
+            name: 'Happy Little Fern',
             watering_history: plant1Date.setDate(plant1Date.getDate() - 6),
             picture_url: 'https://assets.weimgs.com/weimgs/ab/images/wcm/products/202040/0048/faux-potted-green-fern-plant-o.jpg',
             location: 'Bookshelf',
             watering_goal_in_days: 5},
         {
             id: 2,
-            name: 'adorablefern',
+            name: 'Fiddly Fig',
             watering_history: plant2Date.setDate(plant2Date.getDate() - 1),
             picture_url: 'https://cdn.shopify.com/s/files/1/0013/3529/6118/products/LBE-14-LBE-CYL-14-WHT_Fiddle-Leaf-Fig-Tree-14.jpg?v=1590448760',
             location: 'porch',
